cbd9ea527a84996623f75b340eee9a263ae8bca5
RafaelDanelluci <rafaeldanelluci@yahoo.com.br>
2022-02-01 10:03:00 -0300

    Completed

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Ifood</h1>
+      <Ifoodcounter />
+    </>
+  );
+}
+
+export default App;
